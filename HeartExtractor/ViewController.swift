d1a3f570ece499b62cc46c42bf75928a9436232a
김승호 <niceb5y@gmail.com>
2016-04-07 15:33:59 +0900

    Initial Commit

---
+++ HeartExtractor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HeartExtractor
+//
+//  Created by 김승호 on 2016. 4. 7..
+//  Copyright © 2016년 Seungho Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
